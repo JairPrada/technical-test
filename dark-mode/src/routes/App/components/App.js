2cd7ff027473408fb489f5641b8348bdfecb2b2a
jairprada <jairprada9@gmail.com>
2021-07-15 08:33:17 -0500

    Second point completetd

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import '../styles/_app.scss';
+import { useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import "../styles/_app.scss";
 
 function App() {
+  const mainDiv = useRef(null);
+  const [mode, setMode] = useState({
+    icon: faMoon,
+    color: "#000000",
+  });
+  const [spin, setSpin] = useState(false);
+  const changeMode = () => {
+    mainDiv.current.classList.toggle("dark-mode");
+    setSpin(true);
+    if (mode.icon === faMoon) {
+      setMode({ icon: faSun, color: "#FFA500" });
+    } else {
+      setMode({ icon: faMoon, color: "#000000" });
+    }
+    setInterval(() => {
+      setSpin(false);
+    }, 4000);
+  };
   return (
-    <div className="app">
+    <div ref={mainDiv} className="app">
       <div className="level">
         <div>
           <h1 className="title">Dark Mode Challenge</h1>
@@ -13,36 +31,53 @@ function App() {
 
         {/* --The button that should toggle dark mode-- */}
         <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+          <FontAwesomeIcon
+            icon={mode.icon}
+            spin={spin}
+            style={{ color: mode.color }}
+            onClick={changeMode}
+          />
         </button>
-
       </div>
-
       <div className="columns">
         <div className="column">
-          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
         </div>
         <div className="column">
-          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
         </div>
       </div>
-
       <div className="field">
         <div className="control">
           <input className="input" type="text" placeholder="Name" />
         </div>
       </div>
-
       <div className="field">
         <div className="control">
           <input className="input" type="text" placeholder="Email" />
         </div>
       </div>
-
       <section className="section">
         <div className="buttons level-right">
-          <a className="button is-primary">Save</a>
-          <a className="button is-link">Submit</a>
+          <a href="/" className="button is-primary">
+            Save
+          </a>
+          <a href="/" className="button is-link">
+            Submit
+          </a>
         </div>
       </section>
     </div>
